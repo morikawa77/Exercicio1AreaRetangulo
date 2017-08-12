3af37958223f0849012413c7412364f23653a311
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 04:00:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio1AreaRetangulo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int baseR, alturaR, areaR;
+			Console.WriteLine("Digite o tamanho da base em metros: ");
+			baseR = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Digite o tamanho da altura em metros: ");
+			alturaR = Convert.ToInt32(Console.ReadLine());
+			areaR = baseR * alturaR;
+			Console.WriteLine("A área do retânagulo é de {0} metros.", areaR);
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Exercício 1 - Calcular área de retângulo C# Curso Programação de Dispositivos Móveis SENAC
